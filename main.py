2a32d3521315921a076092e65206b5e9116e4a95
GriMel <GriefMontana@gmail.com>
2016-06-26 15:40:22 +0300

    Almost done algorythm for future module

--- main.py
+++ main.py
@@ -2,8 +2,10 @@
 # !/usr/bin/env
 
 import requests
-from PyQt4 import QtGui, QtCore
-from PyQt4.QtCore import pyqtSignal
+import re
+from bs4 import BeautifulSoup as bs
+from time import sleep
+from itertools import product
 
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1;' +
@@ -30,15 +32,66 @@ def checkRegions():
         }
 
 
+def findCommon(string1, string2):
+    """
+    Function to retrieve the current processing currency
+    """
+    set1 = set(string1.split(' '))
+    set2 = set(string2.split(' '))
+    common_words = set1.intersection(set2)
+    count_of_words = len(common_words)
+    # Idea
+    # ('A', 'B')
+    # product generates ('A', 'A'), ('B', 'B')
+    # By calling set(combination) we leave only unique combinations
+    combinations = [combination for combination in
+                    product(common_words, repeat=count_of_words)
+                    if len(set(combination)) == count_of_words]
+    # Retrieve the currency by checking exact
+    # string in string1
+    for comb in combinations:
+        string = " ".join(comb)
+        if re.search(string, string1):
+            return string
+
+
 def main():
     """
     Main module for processing GUI
     """
-    url = 'http://www.investing.com/currencies/Service/currency?region_ID={0}&currency_ID={1}'
-    region_id = 4
-    currency_id = 93
-    response = requests.get(url=url.format(region_id, currency_id),
-                            headers=HEADERS)
+    # url = 'http://www.investing.com/currencies/Service/currency?region_ID={0}&currency_ID={1}'
+    url2 = 'http://www.investing.com/currencies/Service/currency?currency_ID={0}'
+    array = []
+    regions = []
+    for currency_id in range(1, 3):
+        subarray = []
+        try:
+            response = requests.get(url=url2.format(currency_id),
+                                    headers=HEADERS)
+            soup = bs(response.text, "lxml")
+            string1, string2 = soup('a')[:2]
+            currency_name = findCommon(string1, string2)
+            for ul in soup('ul'):
+                items = [{'link': a['href'],
+                          'title': a['title'],
+                          'name': a.text} for a in ul('a')]
+                region_name = ul.span_string
+                region = {'region_name': region_name,
+                          'currencies': []}
+
+                for a in ul('a'):
+                    link = a['href']
+                    title = a['title']
+                    name = a.text
+                subarray.append({'region': region,
+                                 'items': items
+                                 })
+        except:
+            pass
+        array.append({'currency_id': currency_id,
+                      'currency_name': currency_name,
+                      'items': subarray})
+
 
 if __name__ == "__main__":
 
